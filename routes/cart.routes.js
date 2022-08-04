39f6d99dd2e2cbf79cbc968c5f5b49e40c52cd42
Nokeen <cordrepayne@gmail.com>
2022-08-04 09:33:46 -0500

    setting up to heroku

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,17 +2,26 @@ const router = require('express').Router();
 // const express = require('express');
 const User = require("../models/User.model");
 const Product = require("../models/Product.model");
+const { useResolvedPath } = require('react-router-dom');
 // const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
 
-router.put('/cart', (req,res)=>{
+router.post('/cart', (req,res)=>{
     const { productId, userId } = req.body;
     Product.findById(productId)
-    .then(foundProduct => {
-        User.findByIdAndUpdate(userId, { $push: { cart: foundProduct}})
-        .then(response => res.json(response))
+        .then(foundProduct => {
+            User.findByIdAndUpdate(userId, { $push: { cart: {foundProduct, newId: mongoose.Types.ObjectId}}})
+                .then(response => res.json(response))
+                .catch(err => res.json(err))
+        })
         .catch(err => res.json(err))
-    })
-    .catch(err => res.json(err))
 })
 
+router.put('/cart', (req,res)=>{
+    const { index, userId } = req.body;
+    User.findById(userId)
+        .then(foundUser => {
+            foundUser.cart.splice(index, 1)
+        })
+        .catch(err => res.json(err))
+})
 module.exports = router;
